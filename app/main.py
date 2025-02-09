8c2398941aacce32d319b203ae5f38fb7ac452f6
dhanush hebbar <dhanush@kali.kali>
2025-02-09 12:55:23 +0530

    setting up for db integration

--- app/main.py
+++ app/main.py
@@ -1,25 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
+
+from app.api.movies import movies
 
 app = FastAPI()
 
-fake_movie_db = [
-    {
-        'name': 'Star Wars: Episode IX - The Rise of Skywalker',
-        'plot': 'The surviving members of the resistance face the First Order once again.',
-        'genres': ['Action', 'Adventure', 'Fantasy'],
-        'casts': ['Daisy Ridley', 'Adam Driver']
-    }
-]
-
-class Movie(BaseModel):
-    name: str
-    plot: str
-    genres: List[str]
-    casts: List[str]
-
-
-@app.get('/', response_model=List[Movie])
-async def index():
-    return fake_movie_db
+app.include_router(movies)
